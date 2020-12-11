04e0838141c85061a60a365e64047f80f9b2050f
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-11 23:10:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+document.getElementById('mybtn').addEventListener('click', calculatearea)
+
+function calculatearea () {
+  let a = 0
+  let b = 0
+  let height = 0
+  let area = 0
+  a = document.getElementById('a').value
+  b = document.getElementById('b').value
+  height = document.getElementById('height').value
+  area = (a + b) * parseFloat(height) /2
+  alert(area)
+}
+  
